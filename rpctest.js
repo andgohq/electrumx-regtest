657d9eb130491e3f3ff3e339750e38e0e6ed81fa
Toshihide Hara <toshihidehr@gmail.com>
2019-04-04 17:59:01 +0900

    Update rpctest.js

--- rpctest.js
+++ rpctest.js
@@ -1,13 +1,16 @@
 const ElectrumCli = require('electrum-client')
 const main = async () => {
     // const ecl = new ElectrumCli(995, 'btc.smsys.me', 'tls') // tcp or tls
+    // const ecl = new ElectrumCli(50001, 'electrum.coinucopia.io', 'tcp') // tcp or tls
     const ecl = new ElectrumCli(55001, 'localhost', 'tcp') // tcp or tls
     await ecl.connect(); // connect(promise)
     ecl.subscribe.on('blockchain.headers.subscribe', (v) => console.log(v)) // subscribe message(EventEmitter)
     try{
-        const ver = await ecl.server_version("electrum-client", "1.4") // json-rpc(promise)
+        const ver = await ecl.server_version("electrum-client", "1.2") // json-rpc(promise)
         console.log(ver)
-    }catch(e){
+        const response = await ecl.blockchainBlock_getHeader('570144');
+        console.log(response);
+        }catch(e){
         console.log(e)
     }
     await ecl.close() // disconnect(promise)
